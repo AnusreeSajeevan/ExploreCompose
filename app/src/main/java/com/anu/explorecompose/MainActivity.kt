049a71d97af9a2d3a5afacae3510a76bdc1238df
Anusree Sajeevan <anusreesajeevan@Anusrees-MacBook-Pro.local>
2022-02-17 15:46:35 +0530

    Add button and refactor

--- app/src/main/java/com/anu/explorecompose/MainActivity.kt
+++ app/src/main/java/com/anu/explorecompose/MainActivity.kt
@@ -3,14 +3,11 @@ package com.anu.explorecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.anu.explorecompose.ui.theme.ExploreComposeTheme
@@ -19,35 +16,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MyApp()
+            MyApp()
         }
     }
 }
 
 @Composable
-private fun MyApp() {
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     ExploreComposeTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            Greeting("Android")
+            Column(modifier = Modifier.padding(vertical = 4.dp)) {
+                for (name in names)
+                    Greeting(name)
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .fillMaxWidth(1f)
+    ) {
+        Row(Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = stringResource(R.string.greeting))
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(R.string.show_more_button))
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(widthDp = 320)
 @Composable
 fun DefaultPreview() {
-    ExploreComposeTheme {
-        Greeting("Android")
+    ExploreComposeTheme() {
+        MyApp()
     }
 }
