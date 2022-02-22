2be9beff5921f66af9c5c70d83edce4b8e10cbcb
Anusree Sajeevan <anusreesajeevan@Anusrees-MacBook-Pro.local>
2022-02-22 16:28:48 +0530

    Create lazy performant list

--- app/src/main/java/com/anu/explorecompose/MainActivity.kt
+++ app/src/main/java/com/anu/explorecompose/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -33,14 +35,15 @@ private fun MyApp() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
+fun Greetings(names: List<String> = List(1000) {"$it"} ) {
     ExploreComposeTheme {
         Surface(
             color = MaterialTheme.colors.background
         ) {
-            Column(modifier = Modifier.padding(vertical = 4.dp)) {
-                for (name in names)
+            LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+                items(items = names) { name ->
                     Greeting(name)
+                }
             }
         }
     }
