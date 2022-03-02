5a6a001b2c722a256ee3179c3ba1d89fc415dd61
Anusree Sajeevan <anusreesajeevan@Anusrees-MacBook-Pro.local>
2022-03-02 12:22:33 +0530

    Use material theme color for image border and title

--- app/src/main/java/com/anu/explorecompose/MainActivity.kt
+++ app/src/main/java/com/anu/explorecompose/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -18,6 +21,8 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -25,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.anu.explorecompose.domain.Message
 import com.anu.explorecompose.ui.theme.BasicsCodelabTheme
+import com.anu.explorecompose.ui.theme.ExploreComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -152,12 +158,17 @@ fun createChatView() {
 
 @Composable
 fun MessageCard(message: Message) {
-    Row(modifier = Modifier.padding(8.dp)){
-        Image(painter = painterResource(id = R.drawable.img), contentDescription = "user",
-            modifier = Modifier.size(30.dp))
-        Column {
-            Text(text = message.author)
-            Text(text = message.message)
+    ExploreComposeTheme {
+        Row(modifier = Modifier.padding(8.dp)){
+            Image(painter = painterResource(id = R.drawable.img), contentDescription = "user",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, MaterialTheme.colors.surface, CircleShape))
+            Column {
+                Text(text = message.author, color = MaterialTheme.colors.surface)
+                Text(text = message.message)
+            }
         }
     }
 }
@@ -165,7 +176,9 @@ fun MessageCard(message: Message) {
 @Preview(showBackground = true, heightDp = 200, widthDp = 200)
 @Composable
 fun MessageWindowPreview() {
-    createChatView()
+    ExploreComposeTheme {
+        createChatView()
+    }
 }
 
 /*
